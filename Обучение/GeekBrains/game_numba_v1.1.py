bf73a352838b536cd35d4db68caf211468622e3e
FoxyWRTH <f0xxxywtf@gmail.com>
2022-09-07 05:30:42 +0500

    Модифицировал игру, удалил цвета. Создал игру "Угадай число" но наоборот. Угадывает компьютер.

--- Обучение/GeekBrains/game_numba_v1.1.py
+++ Обучение/GeekBrains/game_numba_v1.1.py
@@ -1,21 +1,18 @@
 # Игра угадай число.
 import random
-from colorama import *
 
-player_try_count = input(Fore.GREEN + 'Введите желаемое количество попыток. \n' + Style.RESET_ALL)
-player_range_count = input(Fore.GREEN + 'Введите диапазон чисел. \n' + Style.RESET_ALL)
+player_try_count = input('Введите желаемое количество попыток. \n')
+player_range_count = input('Введите диапазон чисел. \n')
 
 while True:
-    if player_try_count == 'exit' or player_try_count == 'выход':
-        exit()
-    elif player_range_count == 'exit' or player_range_count == 'выход':
+    if player_try_count in ('exit', 'выход') or player_range_count in ('exit', 'выход'):
         exit()
     elif player_try_count.isdigit() and player_range_count.isdigit():
         break
     else:
-        print(Fore.RED + 'Введи целое число. И только его. Давай по новой...\n' + Style.RESET_ALL)
-        player_try_count = input(Fore.GREEN + 'Введите желаемое количество попыток. \n' + Style.RESET_ALL)
-        player_range_count = input(Fore.GREEN + 'Введите диапазон чисел. \n' + Style.RESET_ALL)
+        print('Введи целое число. И только его. Давай по новой...\n')
+        player_try_count = input('Введите желаемое количество попыток. \n')
+        player_range_count = input('Введите диапазон чисел. \n')
 
 pc_random_try = random.randrange(0, int(player_range_count))
 # print(pc_random_try)
@@ -23,22 +20,17 @@ pc_random_try = random.randrange(0, int(player_range_count))
 for i in range(int(player_try_count)):
     player_try = input('Введите число \n')
     if player_try == 'exit' or player_try == 'выход':
-        print(Fore.CYAN + 'Победа достаётся только упорным!' + Style.RESET_ALL)
+        print('Победа достаётся только упорным!')
         exit()
     elif not player_try.isdigit():
-        print(Fore.RED + 'Вводи только целые числа. Кстати у тебя осталось ' + Style.RESET_ALL + Fore.YELLOW
-              + f'{int(player_try_count) - i - 1}' + Style.RESET_ALL + Fore.RED + ' попыток! ;)' + Style.RESET_ALL)
+        print(f'Вводи только целые числа. Кстати у тебя осталось {int(player_try_count) - i - 1} попыток! ;)')
         i = + 1
     elif int(player_try) == pc_random_try:
-        print(Fore.CYAN + f'Ты победил бля! Всего то за {i} попыток!' + Style.RESET_ALL)
+        print(f'Ты победил бля! Всего то за {i} попыток!')
         break
     elif int(player_try) > pc_random_try:
-        print(Fore.GREEN + f'Ты ввел больше чем нужно, у тебя осталось ' + Style.RESET_ALL +
-              Fore.YELLOW + f'{int(player_try_count) - i - 1}' + Style.RESET_ALL +
-              Fore.GREEN + ' попыток.' + Style.RESET_ALL)
+        print(f'Ты ввел больше чем нужно, у тебя осталось {int(player_try_count) - i - 1} попыток.')
         i = + 1
     elif int(player_try) < pc_random_try:
-        print(Fore.GREEN + f'Ты ввел меньше чем нужно, у тебя осталось ' + Style.RESET_ALL +
-              Fore.YELLOW + f'{int(player_try_count) - i - 1}' + Style.RESET_ALL +
-              Fore.GREEN + ' попыток.' + Style.RESET_ALL)
+        print(f'Ты ввел меньше чем нужно, у тебя осталось {int(player_try_count) - i - 1} попыток.')
         i = + 1
