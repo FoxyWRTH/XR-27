0db621369a68a64212b4645ac59c2dd38237d28c
FoxyWRTH <f0xxxywtf@gmail.com>
2022-09-08 09:02:25 +0500

    Переделал код в играх, узнал кое-что новое. Теперь код стал лучше.

--- Обучение/GeekBrains/game_numba_reverse_v1.1.py
+++ Обучение/GeekBrains/game_numba_reverse_v1.1.py
@@ -2,37 +2,31 @@
 
 import random
 
-# Ниже — Врата. Они отсекают возможность внести что-либо, кроме цифр.
-player_range = input('Введи диапазон. Используйте только цифры.\n')
-player_number = input('Введи своё число которое будет пытаться отгадать компьютер. Используйте только цифры.\n')
-pc_try_count = input('Сколько попыток будет у компьютера? Используйте только цифры.\n')
-
 while True:
-    if player_number in ('exit', 'выход') or pc_try_count in ('exit', 'выход') or player_range in ('exit', 'выход'):
-        exit()
-    elif player_range.isdigit() and player_number.isdigit() and pc_try_count.isdigit():
+    try:
+        player_range = int(input('Введи диапазон. Используйте только цифры.\n'))
+        pc_try_count = int(input('Сколько попыток будет у компьютера? Используйте только цифры.\n'))
+        player_number = int(input('Введи своё число которое будет пытаться отгадать компьютер. '
+                                  'Используйте только цифры.\n'))
         break
-    else:
+    except ValueError:
         print('Используйте только целые цифры.')
-        player_range = input('Введи диапазон. Используйте только цифры.\n')
-        player_number = input('Введи своё число которое будет пытаться отгадать компьютер. Используйте только цифры.\n')
-        pc_try_count = input('Сколько попыток будет у компьютера? Используйте только цифры.\n')
 
 min_number = 1
 max_number = int(player_range)
 try_count = 1
 
-for i in range(1, int(pc_try_count) + 1):
+for i in range(1, pc_try_count + 1):
     pc_try_catch = random.randint(min_number, max_number)
     print(f'Компьютер полагает что ваше число: {pc_try_catch}')
     print(f'Если вы вдруг забыли, вы загадали {player_number}')
-    print(f'Осталось попыток: {int(pc_try_count) - i}')
+    print(f'Осталось попыток: {pc_try_count - i}')
     correction = input('Введите корректировку\n')
 
-    if int(player_number) == int(pc_try_catch):
+    if int(player_number) == pc_try_catch:
         print(f'Компьютер победил. Попыток использовано компьютером для отгадывания числа: {i}')
         exit()
-    elif try_count == int(pc_try_count):
+    elif try_count == pc_try_count:
         print('Компьютер, истратив все доступные ему попытки проиграл. Гордая победа Человека над машиной!')
         exit()
     elif correction == '>':
--- Обучение/GeekBrains/game_numba_v1.1.py
+++ Обучение/GeekBrains/game_numba_v1.1.py
@@ -1,33 +1,28 @@
 # Игра угадай число.
 import random
 
-player_try_count = input('Введите желаемое количество попыток. \n')
-player_range_count = input('Введите диапазон чисел. \n')
-
 while True:
-    if player_try_count in ('exit', 'выход') or player_range_count in ('exit', 'выход'):
-        exit()
-    elif player_try_count.isdigit() and player_range_count.isdigit():
+    try:
+        player_range_count = int(input('Введите диапазон чисел. \n'))
+        player_try_count = int(input('Введите желаемое количество попыток. \n'))
         break
-    else:
-        print('Введи целое число. И только его. Давай по новой...\n')
-        player_try_count = input('Введите желаемое количество попыток. \n')
-        player_range_count = input('Введите диапазон чисел. \n')
+    except ValueError:
+        print('Fuck You!')
 
 pc_random_try = random.randrange(0, int(player_range_count))
 # print(pc_random_try)
 
-for i in range(int(player_try_count)):
+for i in range(player_try_count):
     player_try = input('Введите число \n')
     if player_try == 'exit' or player_try == 'выход':
         print('Победа достаётся только упорным!')
         exit()
     elif not player_try.isdigit():
-        print(f'Вводи только целые числа. Кстати у тебя осталось {int(player_try_count) - i - 1} попыток! ;)')
+        print(f'Вводи только целые числа. Кстати у тебя осталось {player_try_count - i - 1} попыток! ;)')
     elif int(player_try) == pc_random_try:
-        print(f'Ты победил бля! Всего то за {i} попыток!')
+        print(f'Молодец, ты победил. Использовано попыток: {i}')
         break
     elif int(player_try) > pc_random_try:
-        print(f'Ты ввел больше чем нужно, у тебя осталось {int(player_try_count) - i - 1} попыток.')
+        print(f'Ты ввел больше чем нужно, у тебя осталось {player_try_count - i - 1} попыток.')
     elif int(player_try) < pc_random_try:
-        print(f'Ты ввел меньше чем нужно, у тебя осталось {int(player_try_count) - i - 1} попыток.')
+        print(f'Ты ввел меньше чем нужно, у тебя осталось {player_try_count - i - 1} попыток.')
