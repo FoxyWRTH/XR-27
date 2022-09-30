64a112d6b09df21a0eb366171ecafb685e4de59a
FXR-34 <f0xxxywtf@gmail.com>
2022-10-01 02:33:53 +0500

    Завершил 5ый вебинар.

--- EDUCATION/GeekBrains/first_quarter/webinar_04.py
+++ EDUCATION/GeekBrains/first_quarter/webinar_04.py
@@ -3,32 +3,50 @@
 """
 
 import re
+import datetime
 import requests
+
+# Task - 2/3
+
+# Написать функцию currency_rates(), принимающую в качестве аргумента код
+# валюты (например, USD, EUR, GBP, ...) и возвращающую курс этой валюты по
+# отношению к рублю. Использовать библиотеку requests. В качестве API можно
+# использовать http://www.cbr.ru/scripts/XML_daily.asp
+# *
+# Доработать функцию currency_rates(): теперь она должна возвращать кроме
+# курса дату, которая передаётся в ответе сервера. Дата должна быть в виде
+# объекта date. Подумайте, как извлечь дату из ответа, какой тип данных лучше
+# использовать в ответе функции?
 
 URL = 'https://www.cbr.ru/scripts/XML_daily.asp'
 answer = requests.get(URL, timeout=10).text
 
-finding = re.split('>|<|</|/', answer)
+finding = re.split('>|<|</|/', answer)  # Распиливаю общую массу информации.
+
+finding_date = finding[3].split('"')[1].split('.')  # Изолирую дату.
+
+date = datetime.date(int(finding_date[2]),  # Помещаю части даты
+                     int(finding_date[1]),  # В нужные отсеки.
+                     int(finding_date[0]))
 
 
 def current_rates(money_code):
     """
     :param money_code: литерал валюты
-    :return собранные куски ответа сайта в удобоваримом виде.
+    :return Форматированный ответ сайта в удобоваримом виде.
     """
-    if money_code.upper() in finding:
+    if money_code.upper() in finding:  # Убираю зависимость регистра.
         index = finding.index(money_code.upper())  # Нахождение индекса валюты.
-        name_money = finding[index + 10]  # Название валюты.
-        value_money = finding[index + 5]  # Количество валюты.
+        name_money = finding[index + 10]  # Нахождение название валюты.
+        value_money = finding[index + 5]  # Нахождение количество валюты.
         code_money = finding[index]  # Аббревиатура валюты.
         russian_rub = finding[index + 15].replace(',', '.')  # Сумма в рублях.
-        result_date = finding[3].split('"')[1]  # Дата.
         result = f'Вы выбрали: "{name_money}"\n{value_money} единица "' \
                  f'{code_money}" = {float(russian_rub)} "RUB"\n' \
-                 f'Дата запроса: {result_date}'
+                 f'Дата запроса: {date}'
         return result
     return None
 
 
 if __name__ == '__main__':
-    print(current_rates('cny'))
+    print(current_rates('eur'))
---
+++ EDUCATION/GeekBrains/first_quarter/webinar_05.py
@@ -0,0 +1,97 @@
+"""
+Домашняя работа. Лекция №5
+"""
+
+
+# Task - 1
+# Написать генератор нечётных чисел от 1 до n (включительно)
+# Используя ключевое слово yield.
+
+
+def odd_generator_yield(max_n):
+    """
+    :param Максимальное число.
+    :return набор нечётных чисел до максимального числа.
+    """
+    for i in range(1, max_n + 1):
+        if i % 2 != 0:
+            yield i
+
+
+odd_list_yield = odd_generator_yield(10 ** 6)
+print(max(odd_list_yield))
+
+# Task - 2
+# Решить задачу генерации нечётных чисел от 1 до n (включительно)
+# Не используя ключевое слово yield.
+MAX_N = 10 ** 6
+odd_list = [i for i in range(1, MAX_N + 1) if i % 2 != 0]
+print(max(odd_list))
+
+# Task - 3
+# Необходимо реализовать генератор, возвращающий кортежи вида (<tutor>, <klass>)
+# Например:
+# ('Иван', '9А')
+# ('Анастасия', '7В')
+# ...
+# Количество генерируемых кортежей не должно быть больше длины списка tutors.
+# Если в списке klasses меньше элементов, чем в списке tutors
+# Необходимо вывести последние кортежи в виде: (<tutor>, None)
+# Например:
+# ('Станислав', None)
+
+tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
+klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '11А', '3В']
+
+
+def union_generator(union_1, union_2):
+    """
+    :param union_1: 1 список.
+    :param union_2: 2 список.
+    :return кортежи из объединения списков.
+    """
+    len_1 = len(union_1)
+    len_2 = len(union_2)
+    if len_1 < len_2:
+        for _ in range(len_2 - len_1):
+            union_1.append(None)
+    elif len_2 < len_1:
+        for _ in range(len_1 - len_2):
+            union_2.append(None)
+    for index, element in enumerate(tutors):
+        result = (element, klasses[index])
+        yield result
+
+
+generator_result = union_generator(tutors, klasses)
+for i in generator_result:
+    print(i)
+
+# Task - 4.
+
+# Представлен список чисел.
+# Необходимо вывести те его элементы, значения которых больше предыдущего
+# Например:
+# src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+# result = [12, 44, 4, 10, 78, 123]
+
+some_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+result_list = []
+
+for i in range(1, len(some_list)):
+    if some_list[i] > some_list[i - 1]:
+        result_list.append(some_list[i])
+
+print(result_list)
+
+# Task - 5.
+# Подумайте, как можно сделать оптимизацию кода по памяти, по скорости.
+# Представлен список чисел. Определить элементы списка, не имеющие повторений.
+# Сформировать из этих элементов список с сохранением порядка их следования
+# в исходном списке, например:
+# result = [23, 1, 3, 10, 4, 11]
+
+source = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+print(source)
+result = [i for i in source if source.count(i) == 1]
+print(result)
