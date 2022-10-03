e50e0f6937950a389b1fe74a71bf063084d28402
FXR-34 <f0xxxywtf@gmail.com>
2022-10-03 07:00:52 +0500

    Частично 6ой сделал.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,15 +1,9 @@
 
-y
-IEDUCATION/GeekBrains/introduction_course/Json and Pickle/dump and Load.py,1\4\1407bb965b3ad6ca20f8de233d2d7116adad89cb
 Q
 !EDUCATION/My_Firs_Site/index.html,7\a\7ab1b033c1aaaa38f658c8b221cd428ab57f8361
-`
-0EDUCATION/GeekBrains/first_quarter/webinar_02.py,5\e\5e0488aa2a00f0737480ec22543e5eb070307c36
 w
 GEDUCATION/GeekBrains/introduction_course/Modules/education_Module_01.py,a\8\a8714a4ac5a60f6ffd3ce56f726b59399282276e
 w
 GEDUCATION/GeekBrains/introduction_course/Primitive_File_Manager/core.py,2\c\2c8a3855b348697e1d5e57a54455ba12eb762aa3
 w
 GEDUCATION/GeekBrains/introduction_course/Primitive_File_Manager/main.py,3\a\3a7fe33c29442edcf27c1ea5608509b7863b51b2
-b
-2EDUCATION/GeekBrains/first_quarter/webinar_03_n.py,6\0\60d915b6781cacb7f1ada6d81a33dc1f318aaeba
---
+++ EDUCATION/GeekBrains/first_quarter/webinar_06.py
@@ -0,0 +1,53 @@
+"""
+Домашняя работа. Лекция №6
+"""
+
+from collections import Counter
+import re
+
+# Task - 1/2
+# 1. Не используя библиотеки для парсинга, распарсить
+# (получить определённые данные) файл логов web-сервера nginx_logs.txt
+# получить список кортежей (<remote_addr>, <request_type>, <requested_resource>)
+# Например:
+# [
+# ...
+# ('141.138.90.60', 'GET', '/downloads/product_2'),
+# ('141.138.90.60', 'GET', '/downloads/product_2'),
+# ('173.255.199.22', 'GET', '/downloads/product_2'),
+# ...
+# ]
+# 2. * (вместо 1) Найти IP адрес спамера и количество отправленных им запросов
+# по данным файла логов из предыдущего задания.
+# Примечания: спамер — это клиент, отправивший больше всех запросов.
+
+# Заметки: Что бы возвращать цельное значение в кортеже используй ("str",)
+path_to_file = 'C:/Users/FXR-34/Desktop/nginx_logs.txt'
+ip = []
+with open(path_to_file, 'r', encoding='utf-8') as work_file:
+    for line in work_file:
+        remote_addr = (re.split('"| - - | ', line)[0],)
+        request_type = (re.split('"| - - | ', line)[4],)
+        requested_resource = (re.split('"| - - | ', line)[5],)
+        print(f'{remote_addr}'
+              f'{request_type}'
+              f'{requested_resource}'
+              .replace(',', ''))
+        ip.append(remote_addr)
+
+# С помощью "Counter" уточняю максимальное количество запросов.
+max_count_of_request = max(Counter(ip).values())
+
+
+def get_spam_id(request_count):
+    """
+    :param request_count: Передаём максимальное значение запросов.
+    :return ИД с наибольшим количеством запросов.
+    """
+    for key, value in Counter(ip).items():
+        if request_count == value:
+            return key
+
+
+print(f'User with ID {get_spam_id(max_count_of_request)} '
+      f'made the most requests: {max_count_of_request}'.replace(',', ''))
