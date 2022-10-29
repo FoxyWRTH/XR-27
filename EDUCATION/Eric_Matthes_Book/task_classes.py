b711cf6730aadde888cca4239d975f4727c40311
FXR-34 <f0xxxywtf@gmail.com>
2022-10-29 20:42:42 +0500

    Вроде усвоил классы.

--- EDUCATION/Eric_Matthes_Book/task_classes.py
+++ EDUCATION/Eric_Matthes_Book/task_classes.py
@@ -206,3 +206,161 @@ print(f'Ah! WTF is that? Why do you exist? '
       f'{sub_model_01.bot_name}, answer me!')
 print(sub_model_01.answer())
 print(sub_model_01.os_system)
+
+
+# Попытки входа. (9.5)
+# Добавьте атрибут login_attempts в класс User.
+# Напишите метод increment_login_attempts(), увеличивающий значение
+# login_attempts на 1. Напишите другой метод с именем reset_login_attempts(),
+# обнуляющий значение login_attempts. Создайте экземпляр класса User и вызовите
+# increment_login_attempts() несколько раз. Выведите значение login_attempts,
+# чтобы убедиться в том, что значение было изменено правильно, а затем вызовите
+# reset_login_attempts(). Снова выведите login_attempts и убедитесь в том, что
+# значение обнулялось.
+
+class UserInfo():
+    """Класс доп. информации"""
+
+    def __init__(self, location='Russia', work='Programmer', color='Red'):
+        """Инициализация атрибутов"""
+        self.location = location
+        self.work = work
+        self.color = color
+
+    def user_location(self):
+        """Расположение пользователя"""
+        print(f'User location: {self.location}')
+
+    def user_work(self):
+        """Работа пользователя"""
+        print(f'User work: {self.work}')
+
+    def user_color(self):
+        """Цвет категории пользователя"""
+        print(f'User location: {self.color}')
+
+
+class UserF():
+    """Условный пользователь"""
+
+    def __init__(self, name):
+        self.name = name
+        self.counter = 0
+        self.info = UserInfo()
+
+    def log_in(self):
+        """Вход в систему"""
+        self.counter += 1
+        print(f'User wit name: {self.name} log in')
+        print(f'Counter = {self.counter}')
+
+    def reset_log_count(self):
+        """Сброс счётчика"""
+        self.counter = 0
+        print(f'Counter rested. Now counter: {self.counter}')
+
+    def check_counter(self):
+        """Запрос счётчика"""
+        print(f'Counter now: {self.counter}')
+
+
+unit_01 = UserF('Fix')
+
+unit_01.check_counter()
+print()
+for i in range(0, 5):
+    unit_01.log_in()
+print()
+unit_01.check_counter()
+unit_01.reset_log_count()
+unit_01.check_counter()
+print()
+unit_01.info.user_location()
+unit_01.info.user_work()
+unit_01.info.user_color()
+
+
+# Киоск с мороженым. (9.6)
+# киоск с мороженым — особая разновидность ресторана.
+# Напишите класс IceCreamStand, наследующий от класса.
+# Добавьте атрибут с именем flavors для хранения списка сортов мороженого.
+# Напишите метод, который выводит этот список.
+# Создайте экземпляр IceCreamStand и вызовите этот метод.
+
+
+class Shop():
+    """Общий класс для магазинов"""
+
+    def __init__(self, name, location, open_or_close=False):
+        """Инициализация атрибутов"""
+        self.name = name
+        self.location = location
+        self.open = open_or_close
+
+    def shop_info(self):
+        """Запрос информации о магазине"""
+        print(f'Shop name: {self.name}')
+        print(f'Shop location: {self.location}')
+        if self.open is False:
+            print(f'Shop {self.name} now is closed.')
+        else:
+            print(f'Shop {self.name} Open!')
+
+    def open_shop(self):
+        """Открытие магазина"""
+        self.open = True
+        print(f'Now shop with name {self.name} open!')
+
+    def close_shop(self):
+        """Закрытие магазина"""
+        self.open = False
+        print(f'Now shop with name {self.name} closed.')
+
+
+class IceCreamShop(Shop):
+    """Магазин мороженного"""
+
+    def __init__(self, name, location, open_or_close=False):
+        super().__init__(name, location, open_or_close)
+        self.flavors = ['Vanilla', 'Choco', 'Apple']
+        self.toppings = ['nut', 'liquer']
+
+    def get_list_of_flavor(self):
+        """Запрос списка вкусов мороженного"""
+        print('\nAvailable flavors now: ')
+        for element in self.flavors:
+            print(element, end='|')
+
+    def get_list_of_toppings(self):
+        """Запрос списка топингов"""
+        print('\nAvailable toppings now: ')
+        for element in self.toppings:
+            print(element, end='|')
+
+    def adding_flavors(self, adding_f_list):
+        """Добавление вкусов"""
+        self.flavors.extend(adding_f_list)
+
+    def adding_toppings(self, adding_t_list):
+        """Добавление топингов"""
+        self.toppings.extend(adding_t_list)
+
+    def remove_flavors(self, list_f_remove):
+        """Удаление вкусов
+            На вход подавать списки"""
+        for element in list_f_remove:
+            try:
+                if element in self.flavors:
+                    self.flavors.remove(element)
+            except ValueError:
+                pass
+
+    def remove_toppings(self, list_t_remove):
+        """Удаление топингов
+            На вход подавать списки"""
+        for element in list_t_remove:
+            try:
+                if element in self.toppings:
+                    self.toppings.remove(element)
+            except ValueError:
+                pass
