4b90d2054413d8b87084aeecef511c240d46a1a7
Fox <f0xxxywtf@gmail.com>
2022-12-04 23:52:49 +0300

    Завершил перенос конвертера в отдельный репозиторий.

--- EDUCATION/InnoPolis/class_lesson.py
+++ EDUCATION/InnoPolis/class_lesson.py
@@ -4,27 +4,48 @@
 
 
 class Unit773():
+    """Kill Bot"""
+
     def __init__(self, model, serial_number, name):
+        self.can_run = False
+        self.can_walk = False
         self.model = model
         self.serial_number = serial_number
         self.name = name
 
+    def activate_bot(self, can_walk=True, can_run=True):
+        """Activate Kill Bot"""
+        self.can_walk = can_walk
+        self.can_run = can_run
+
+    def deactivate_bot(self, can_walk=False, can_run=False):
+        """Deactivate Kill Bot"""
+        self.can_walk = can_walk
+        self.can_run = can_run
+
 
 class Unit774(Unit773):
+    """Another Kill Bot"""
+
     def __init__(self, model, serial_number, name):
         super().__init__(model, serial_number, name)
+        self.can_fly = True
+        self.can_shot = True
 
 
 if __name__ == '__main__':
     automat_1 = Unit773('U773', 'U773_0101', 'Little Killer')
-    automat_01 = Unit774('U774', 'U774_0102', 'Sub Killer')
+    automat_2 = Unit774('U774', 'U774_0102', 'Sub Killer')
 
-    print(f'You see some automat... Let se...\n'
-          f'{automat_1.model}... '
-          f'{automat_1.name}...'
-          f'{automat_1.serial_number}')
+    print(automat_1.can_run)
+    print(automat_1.can_walk)
 
-    print(f'\nYou see some another automat... Let se...\n'
-          f'{automat_01.model}... '
-          f'{automat_01.name}...'
-          f'{automat_01.serial_number}')
+    automat_1.activate_bot()
+
+    print(automat_1.can_run)
+    print(automat_1.can_walk)
+
+    automat_1.deactivate_bot()
+
+    print(automat_1.can_run)
+    print(automat_1.can_walk)
